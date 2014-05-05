64704468c69885d1abedafb596bdb76d62316ea3
Calibri-Software <calibrisoftware@gmail.com>
2014-05-05 20:15:56 +0300

    Updated.

--- src/math/powers.h
+++ src/math/powers.h
@@ -2,8 +2,6 @@
 #define POWERS_H
 
 //! Std Includes
-#include <type_traits>
-#include <limits>
 #include <cmath>
 
 //! Project Includes
@@ -15,30 +13,21 @@ namespace Calibri
 namespace Powers
 {
 
-namespace Constants
-{
-
-CONSTEXPR static auto multiplier = [](longdouble base, longdouble value) -> longdouble { return value * base; };
-CONSTEXPR static auto divisor = [](longdouble base, longdouble value) -> longdouble { return value / base; };
-
-}
-
 namespace Internal
 {
 
-template<typename Predicate>
-DECL_CONSTEXPR inline longdouble implementation(Predicate predicate, int64 exponent, longdouble base, longdouble value = 1.0e0l)
+DECL_CONSTEXPR inline longdouble implementation(int64 exponent, longdouble base, longdouble value)
 {
     return exponent != 0
-            ? implementation(predicate, exponent >> 1, base * base, (exponent & 1) != 0 ? predicate(base, value) : value)
+            ? implementation(exponent >> 1, base * base, (exponent & 1) != 0 ? base * value : value)
             : value;
 }
 
 DECL_CONSTEXPR inline longdouble implementation(int64 exponent, longdouble base)
 {
     return exponent >= 0
-            ? implementation(Constants::multiplier, exponent, base)
-            : implementation(Constants::divisor, std::abs(exponent), base);
+            ? implementation(exponent, base, 1.0e0l)
+            : 1.0e0l / implementation(std::abs(exponent), base, 1.0e0l);
 }
 
 }
